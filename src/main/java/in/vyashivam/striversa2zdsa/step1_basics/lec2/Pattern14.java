65879c7981445fa2e11d37a140af6a8080e93b20
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-10 18:39:15 +0530

    Solved Step 1 Lec 2 Pattern 14.

---
+++ src/main/java/in/vyashivam/striversa2zdsa/step1_basics/lec2/Pattern14.java
@@ -0,0 +1,15 @@
+package in.vyashivam.striversa2zdsa.step1_basics.lec2;
+
+public class Pattern14 {
+    public static void letterRightTriangle(int n) {
+        for (int i = 1; i <= n ; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+
+        }
+    }
+}
